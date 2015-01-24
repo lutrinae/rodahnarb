474601d253bb7af54945f7166da770f096d1edd1
Ben Crist <ben@magicmoremagic.com>
2015-01-24 12:33:07 -0600

    started building world class

---
+++ core/src/com/gamejam/rn/game/simulation/Character.java
@@ -0,0 +1,5 @@
+package com.gamejam.rn.game.simulation;
+
+public class Character extends KinematicEntity {
+
+}
--- core/src/com/gamejam/rn/game/simulation/RNWorld.java
+++ core/src/com/gamejam/rn/game/simulation/RNWorld.java
@@ -1,5 +1,46 @@
 package com.gamejam.rn.game.simulation;
 
+import java.util.List;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.gamejam.rn.camera.SmoothCamDebugRenderer;
+import com.gamejam.rn.camera.SmoothCamWorld;
+
 public class RNWorld {
 
+	private static final float MIN_TIMESTEP = 0.016f;
+	
+	private List<Entity> entities;
+	
+	
+	private World physicsWorld;
+	private SmoothCamWorld cameraWorld;
+	
+	
+	private OrthographicCamera camera;
+	
+	
+	private Box2DDebugRenderer physicsDebugRenderer;
+	private SmoothCamDebugRenderer cameraDebugRenderer;
+	
+	
+		
+	
+	public RNWorld() {
+		
+		physicsWorld = new World(0, -9.81f, true);
+		
+		cameraWorld = new SmoothCamWorld()
+		
+		
+		
+		
+	}
+	
+	public void update(float dt) {
+		
+	}
+	
 }
